c27c1149a0fc650ab7e2dc2b1a1a0cb2d965aa77
PanosTheo97 <ptheodos@arx.net>
2023-07-18 10:39:37 +0300

    GoogleSignIn setup

--- Thesis Fitness App/Thesis Fitness App/Launch/View/AppDelegate.swift
+++ Thesis Fitness App/Thesis Fitness App/Launch/View/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 let appDelegate: AppDelegate? = UIApplication.shared.delegate as? AppDelegate
 let appFlowCoordinator = AppFlowCoordinator(appDIContainer: AppDIContainer())
@@ -13,28 +14,39 @@ let appViewModel = AppViewModel()
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     // MARK: - Properties
     
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         appViewModel.configureLoader()
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        var handled: Bool
+        
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            return true
+        }
+        
+        return false
+    }
+    
 }
