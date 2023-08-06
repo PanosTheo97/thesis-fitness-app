1608e6898b87a098a5416b3a213f98a7376c6cbd
PanosTheo97 <ptheodos@arx.net>
2023-08-06 15:53:33 +0300

    Changed Macro Text

--- Thesis Fitness App/Thesis Fitness App/Shared/Enums/MacroTypeEnum.swift
+++ Thesis Fitness App/Thesis Fitness App/Shared/Enums/MacroTypeEnum.swift
@@ -33,7 +33,7 @@ enum MacroTypeEnum {
     func getText() -> String {
         switch self {
         case .protein:
-            return "P"
+            return "Pr"
         case .carbonhydrates:
             return "C"
         case .fats:
