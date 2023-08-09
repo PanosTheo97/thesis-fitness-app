ae2d8a9de8acf5814cf2f08d044f0fb0e647507e
PanosTheo97 <ptheodos@arx.net>
2023-08-09 18:59:31 +0300

    Floating Action Button Library

--- Thesis Fitness App/Thesis Fitness App/Scenes/Diet/View/DietViewController.swift
+++ Thesis Fitness App/Thesis Fitness App/Scenes/Diet/View/DietViewController.swift
@@ -6,10 +6,20 @@
 //
 
 import UIKit
+import Floaty
 
 class DietViewController: UIViewController, BaseProtocol {
     
     // MARK: - IBProperties
+    
+    @IBOutlet weak var addMetricsFAB: Floaty! {
+        didSet {
+            addMetricsFAB.buttonColor = .App.mainText
+            addMetricsFAB.plusColor = .label
+            addMetricsFAB.overlayColor = .systemBackground
+            addMetricsFAB.openAnimationType = .slideUp
+        }
+    }
     
     // MARK: - Properties
     
@@ -30,6 +40,7 @@ class DietViewController: UIViewController, BaseProtocol {
         
         self.view.backgroundColor = .systemBackground
         self.addNavigationButtons()
+        self.configureFAB()
     }
     
     private func registerObservers() {
@@ -46,6 +57,34 @@ class DietViewController: UIViewController, BaseProtocol {
     
     func addNavigationButtons() {
         self.navigationItem.addTitle(title: "Lobby_dietTitle".getLocalized())
+    }
+    
+    func configureFAB() {
+        let addMacrosItem = FloatyItem()
+        addMacrosItem.buttonColor = .App.mainText
+        addMacrosItem.title = "Add Macros"
+        addMacrosItem.titleColor = .label
+        addMacrosItem.icon = .init(systemName: "atom")
+        addMacrosItem.iconTintColor = .label
+        addMacrosItem.handler = { _ in
+            print("Add macros")
+            self.addMetricsFAB.close()
+        }
+        
+        let addFoodItem = FloatyItem()
+        addFoodItem.buttonColor = .App.mainText
+        addFoodItem.title = "Add Food"
+        addFoodItem.titleColor = .label
+        addFoodItem.icon = .init(systemName: "carrot.fill")
+        addFoodItem.iconTintColor = .label
+        addFoodItem.handler = { _ in
+            print("Add food")
+            self.addMetricsFAB.close()
+        }
+        
+        self.addMetricsFAB.addItem(item: addMacrosItem)
+        self.addMetricsFAB.addItem(item: addFoodItem)
+        
     }
 }
 
